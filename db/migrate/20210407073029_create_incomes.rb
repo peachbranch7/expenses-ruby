79aadde30b26e75cc519aa509c9752382f364fe1
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-07 16:36:02 +0900

    incomesテーブル作成

--- db/migrate/20210407073029_create_incomes.rb
+++ db/migrate/20210407073029_create_incomes.rb
@@ -1,7 +1,11 @@
 class CreateIncomes < ActiveRecord::Migration[6.0]
   def change
     create_table :incomes do |t|
-
+      t.integer :price, null: false
+      t.string :category, null: false
+      t.date :date, null: false
+      t.string :memo
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
