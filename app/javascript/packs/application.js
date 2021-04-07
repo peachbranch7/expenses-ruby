548460404dc0e03457449653627180afd3a4a379
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-07 18:38:05 +0900

    chartkickとhighchartsをrequire

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("chart.js")
+require("chartkick").use(require("highcharts"))
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
