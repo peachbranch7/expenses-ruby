d2ffb6b66ee7b16746150f9cbd5d3afd067c5753
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-07 17:01:49 +0900

    FactoryBotを定義(income)

--- spec/factories/incomes.rb
+++ spec/factories/incomes.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :income do
-    
+    price {Faker::Number.rand(1..100000000)}
+    category {"給料"}
+    memo {"2月分給料"}
+    date { Faker::Date.between(from: '1930-01-01', to: '2050-12-31') }
+
+    association :user
   end
 end
