b4f3e60a95dea216204c73b6f68968c383bc1be7
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-07 19:00:25 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'incomes#index'
-  resources :incomes, only: [:new, :create]
-  resources :spendings, only: [:new, :create]
+  resources :incomes, only: [:new, :create, :show]
+  resources :spendings, only: [:new, :create, :show]
 end
