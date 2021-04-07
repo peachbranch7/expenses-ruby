26412b44d6e7c08dfc0ac2dfbb1a4367b82c6773
peachbranch777 <peachbranch.0517@gmail.com>
2021-04-07 17:50:19 +0900

    Timeメソッドをrequire

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,7 +1,11 @@
 class IncomesController < ApplicationController
+  require "time"
+
+  
   def index
     @spendings_time = Spending.where(date: Time.now.beginning_of_month..Time.now.end_of_month).where(user_id: current_user.id).order(date: "ASC")
     @incomes_time = Income.where(date: Time.now.beginning_of_month..Time.now.end_of_month).where(user_id: current_user.id).order(date: "ASC")
+    @this_month = Time.new.month
   end
 
   def new
