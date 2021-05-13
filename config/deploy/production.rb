89dee98dee6bc5d48384da20f6c8c9b1521c7d55
peachbranch777 <peachbranch.0517@gmail.com>
2021-05-13 19:32:19 +0900

    server name変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'the-expenses.work', user: 'ec2-user', roles: %w{app db web}
+server '3.113.149.162', user: 'ec2-user', roles: %w{app db web}
