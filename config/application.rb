7ce1686ae9bdac15eae7bc26ecf7e219f0baec42
peachbranch777 <peachbranch.0517@gmail.com>
2021-05-24 14:46:15 +0900

    confing/application.rb編集

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TheExpenses95
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
